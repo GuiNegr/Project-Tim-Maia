c0d96a7da9358244e369e0a006a653e2aa908e51
GuiNegr <GuiNeg.git@outlook.com>
2024-05-18 13:38:07 -0300

    fazendo a conexão

--- Tim Maia/TimMaia/src/Main.java
+++ Tim Maia/TimMaia/src/Main.java
@@ -1,7 +1,9 @@
+import Controler.ConexaoDAO;
 import VIEW.GUI;
 
 public class Main {
     public static void main(String[] args) {
         new GUI();
+        new ConexaoDAO().conectaBd();
     }
 }
---
+++ Tim Maia/TimMaiaProject/src/Model/Produto.java
@@ -0,0 +1,69 @@
+package Model;
+
+import java.util.Date;
+
+public class Produto {
+    private String id;
+    private String nome_produto;
+    private Date validade_produto;
+    private String tipo_produto;
+    private String qtd_produto;
+
+
+    public Produto(String nome_produto, Date validade_produto, String tipo_produto, String qtd_produto) {
+        this.nome_produto = nome_produto;
+        this.validade_produto = validade_produto;
+        this.tipo_produto = tipo_produto;
+        this.qtd_produto = qtd_produto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome_produto() {
+        return nome_produto;
+    }
+
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    public Date getValidade_produto() {
+        return validade_produto;
+    }
+
+    public void setValidade_produto(Date validade_produto) {
+        this.validade_produto = validade_produto;
+    }
+
+    public String getTipo_produto() {
+        return tipo_produto;
+    }
+
+    public void setTipo_produto(String tipo_produto) {
+        this.tipo_produto = tipo_produto;
+    }
+
+    public String getQtd_produto() {
+        return qtd_produto;
+    }
+
+    public void setQtd_produto(String qtd_produto) {
+        this.qtd_produto = qtd_produto;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " +
+                "id: " + 1 + '\'' +
+                "NOME DO PRODUTO: " + nome_produto + '\'' +
+                "VALIDADE DO PRODUTO: " + validade_produto +
+                "TIPO DO PRODUTO: " + tipo_produto + '\'' +
+                "QTD DO PRODUTO: " + qtd_produto + '\'';
+    }
+}
