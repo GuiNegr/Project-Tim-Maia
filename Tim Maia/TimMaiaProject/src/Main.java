649d9b53bf5395876eab5ce1ad50b2033284c430
GuiNegr <GuiNeg.git@outlook.com>
2024-05-18 13:40:37 -0300

    removendo arquivos

--- Tim Maia/TimMaiaProject/src/Main.java
+++
@@ -1,9 +0,0 @@
-import Controler.ConexaoDAO;
-import VIEW.GUI;
-
-public class Main {
-    public static void main(String[] args) {
-        new GUI();
-        new ConexaoDAO().conectaBd();
-    }
-}
